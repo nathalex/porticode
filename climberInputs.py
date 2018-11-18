8ca2fe9835c4e4afde6ded40d4e4dfd0e34f360e
nathalex <nathaliealexandra@gmail.com>
2018-11-18 08:11:47 +0000

    Add files via upload

--- climberInputs.py
+++ climberInputs.py
@@ -1,4 +1,4 @@
 height = int(input("input climber height in cm\n"))
 legLength = int(input("input climber leg length in cm\n"))
-scale = int(input("input scale from image to real life in cm\n"))
+scale = float(input("input scale from image to real life in cm\n"))
 print("in this version, we're taking a default image of climbing and choosing the green route\n")
